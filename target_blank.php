7aa9a549903647506fc389987e6257615bde98c4
Przemyslaw Jaworowski <przemyslaw.jaworowski@gmail.com>
2022-10-18 17:51:44 +0200

    Fix: Returning one link, instead of modifying attributes

--- target_blank.php
+++ target_blank.php
@@ -2,20 +2,20 @@
 /**
  * Plugin Name: Target _blank
  * Description: Adds or creates <code>target = "_ blank"</code> in links on the_content() in single post
- * Version:     1.0.2
+ * Version:     1.0.3
  * Plugin URI:  https://jetplugs.com
  * Author:      jetplugs.com
  * License:     GPL-2.0+
- * Text Domain: tmc_mm
+ * Text Domain: tmc_tb
  * Domain Path: /langugages
  *
  */
 
 
-if ( ! defined( 'ABSPATH' ) ) {
+if ( ! defined( 'ABSPATH' ) )
+{
     die( 'Die, silly human!' );
 }
-
 
 add_filter( 'the_content', 'modifyContent' );
 
@@ -24,36 +24,28 @@ function modifyContent( $content )
 
     $pattern =  '/<a.*?>.*?<\/a>/';
 
-
-    if ( preg_match_all( $pattern, $content )  && class_exists('DOMDocument') && is_single()  )
+    if ( preg_match_all( $pattern, $content )  && class_exists( 'DOMDocument' ) && is_single()  )
     {
 
-        libxml_use_internal_errors( true );
-
-        $matches                   = array();
-        $dom                       = new DOMDocument('1.0', 'UTF-8');
-        $dom->strictErrorChecking  = false;
-        $dom->validateOnParse      = false;
-
-
-        preg_match_all( $pattern, $content, $matches );
-
-        foreach ( $matches[0] as $match )
+        $content = preg_replace_callback( $pattern, function( $matches )
         {
 
-            $dom->loadHTML( mb_convert_encoding($match, 'HTML-ENTITIES', 'UTF-8') );
+            libxml_use_internal_errors( true );
+            $dom                       = new DOMDocument( '1.0', 'UTF-8' );
+            $dom->strictErrorChecking  = false;
+            $dom->validateOnParse      = false;
+            $dom->loadHTML( mb_convert_encoding( $matches[0], 'HTML-ENTITIES', 'UTF-8' ) );
 
             foreach ( $dom->getElementsByTagName( 'a' ) as $item )
             {
-
                 $item->setAttribute( 'target', '_blank' );
             }
 
             libxml_clear_errors();
-        }
 
+            return $dom->saveHTML();
 
-        $content = preg_replace( $pattern, $dom->saveHTML(), $content );
+        } ,$content);
     }
 
     return $content;
